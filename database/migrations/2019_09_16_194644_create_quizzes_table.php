6fe91d898866e99b47e2a005dfa321058d5974dc
Shea <shealavington@outlook.com>
2019-09-19 12:13:17 +0100

    description nullable

--- database/migrations/2019_09_16_194644_create_quizzes_table.php
+++ database/migrations/2019_09_16_194644_create_quizzes_table.php
@@ -19,7 +19,7 @@ class CreateQuizzesTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
