85713bf1059a05b425af9f1e54dd3be571706c93
Shea <shealavington@outlook.com>
2019-09-19 18:25:51 +0100

    added edit test

--- tests/Browser/RoleRestrictedTest.php
+++ tests/Browser/RoleRestrictedTest.php
@@ -86,4 +86,14 @@ class RoleRestrictedTest extends DuskTestCase
         });
     }
 
+    public function testQuizEdit()
+    {
+        $this->browse(function (Browser $browser) {
+            $quizUrl = '/quizzes/d3d29d70-1d25-11e3-8591-034165a3a613';
+            $browser->visit($quizUrl . '/edit')
+                    ->assertPathIs($quizUrl)
+                    ->screenshot('RoleView/QuizEdit');
+        });
+    }
+
 }
