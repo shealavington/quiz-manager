10186eb2c4339b3d853d04ae97265a199ccd2f3d
Shea Lavington <shealavington@gmail.com>
2019-09-20 11:06:45 +0100

    table modifications

--- database/migrations/2019_08_19_000001_create_users_table.php
+++ database/migrations/2019_08_19_000001_create_users_table.php
@@ -19,7 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('role_id')->references('id')->on('roles');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->rememberToken();
             $table->timestamps();
         });
